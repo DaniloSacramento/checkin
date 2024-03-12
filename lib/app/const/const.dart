6363aa18295331f56d778e387be4aa3737b91a87
Leonardo da Vinci Leite Paiva <leodvc82@macbook-pro-leo.local>
2024-03-12 16:03:11 -0300

    ...

--- lib/app/const/const.dart
+++ lib/app/const/const.dart
@@ -1,6 +1,6 @@
 class ConstsApi {
-  //static const String baseUrl = 'http://acesso.triviacloud.com.br';
-  static const String baseUrl = 'http://177.207.241.213:8090';
+  static const String baseUrl = 'http://acesso.triviacloud.com.br';
+ // static const String baseUrl = 'http://177.207.241.213:8090';
   //static const String baseUrl = 'http://192.168.15.160:8090';
   static const String motoristaAuth = '$baseUrl/api/auth/motorista';
   static const String basicAuth = 'Basic bGVvbmFyZG86MTIzNDU2';
@@ -19,10 +19,12 @@ class ConstsApi {
   static const String statusService = '$baseUrl/api/motorista/checkin/status';
   static const String inativarUser = '$baseUrl/api/motorista/inativar';
   static const String excluirNF = '$baseUrl/api/motorista/checkin/excluir/nf';
-  static const String esqueciMinhaSenhaEmail = "$baseUrl/api/motorista/esqueci-minha-senha";
+  static const String esqueciMinhaSenhaEmail = "$baseUrl/api/motorista/esqueci-minha-senha/email";
   static const String esqueciMinhaSenha = '$baseUrl/api/motorista/esqueci-minha-senha/telefone';
-  static const String esqueciMinhaSenhaToken = '$baseUrl/api/motorista/esqueci-minha-senha/token/sms';
+  static const String esqueciMinhaSenhaToken = '$baseUrl/api/motorista/esqueci-minha-senha/token/telefone';
+  static const String esqueciMinhaSenhaTokenEmail = '$baseUrl/api/motorista/esqueci-minha-senha/token/email';
   static const String alterarMinhaSenha = '$baseUrl/api/motorista/alterar-senha';
   static const String redefinirDados = '$baseUrl/api/motorista/meus-dados';
   static const String alterarSenha = '$baseUrl/api/motorista/meus-dados/alterar-senha';
+  static const String alerarSenhaAuthEsqueci = '$baseUrl/api/auth/motorista/alterar-senha';
 }
